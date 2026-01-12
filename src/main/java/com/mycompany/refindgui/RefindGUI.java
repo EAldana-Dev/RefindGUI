3c6556d2665ecf6116143b1ab245028e596c92cf
EAldana-Dev <e.aldana.dev@gmail.com>
2026-01-11 18:35:57 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/refindgui/RefindGUI.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.refindgui;
+
+/**
+ *
+ * @author salchi
+ */
+public class RefindGUI {
+
+    public static void main(String[] args) {
+        Sign sign = new Sign();
+        
+        sign.setLocationRelativeTo(null);
+        sign.setVisible(true);
+        
+    }
+}
